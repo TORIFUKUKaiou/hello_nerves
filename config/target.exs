10a9d08e9edbe12a94ab67183c94d05a083911dc
TORIFUKUKaiou <torifuku.kaiou@gmail.com>
2019-11-12 22:18:15 +0900

    setup WiFi Networking
    https://github.com/nerves-project/nerves_network#wifi-networking

--- config/target.exs
+++ config/target.exs
@@ -42,3 +42,19 @@ config :nerves_init_gadget,
 # Uncomment to use target specific configurations
 
 # import_config "#{Mix.target()}.exs"
+
+config :nerves_network,
+  regulatory_domain: "JP"
+
+key_mgmt = System.get_env("NERVES_NETWORK_KEY_MGMT") || "WPA-PSK"
+
+config :nerves_network, :default,
+  wlan0: [
+    networks: [
+      [
+        ssid: System.get_env("NERVES_NETWORK_SSID"),
+        psk: System.get_env("NERVES_NETWORK_PSK"),
+        key_mgmt: String.to_atom(key_mgmt)
+      ]
+    ]
+  ]
