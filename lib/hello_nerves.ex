667ca77389011759e0fe81eccb687d68cd4db26b
TORIFUKUKaiou <torifuku.kaiou@gmail.com>
2019-11-12 22:01:27 +0900

    Initial commit

---
+++ lib/hello_nerves.ex
@@ -0,0 +1,18 @@
+defmodule HelloNerves do
+  @moduledoc """
+  Documentation for HelloNerves.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> HelloNerves.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
