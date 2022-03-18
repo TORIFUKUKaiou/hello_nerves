7c0bb294d35a8113d1f33bf2aade18325cc178ee
TORIFUKUKaiou <torifuku.kaiou@gmail.com>
2022-03-18 19:23:21 +0900

    bi-chosei

--- lib/qiita/qiita_delika_202203.ex
+++ lib/qiita/qiita_delika_202203.ex
@@ -40,12 +40,12 @@ defmodule Qiita.Qiitadelika202203 do
     キャンペーンの参加記事です。
     テーマ2『データに関する記事を書こう！』に参加しています。
 
-    [Qiita API v2](https://qiita.com/api/v2/docs)を利用させていただいて、「[データに関する記事を書こう！](https://qiita.com/official-events/30be12dd14c0aad2c1c2)」に参加している記事を収拾します。
-    あつまった記事（データ）にあれこれしてみます。
+    [Qiita API v2](https://qiita.com/api/v2/docs)を利用させていただいて、「[データに関する記事を書こう！](https://qiita.com/official-events/30be12dd14c0aad2c1c2)」に参加しているとおもわれる記事を収拾します。
+    あつまった記事群（データ）にあれこれしてみます。
 
-    - LGTM数順に記事を並べています
-    - 投稿者ごとの記事数を集計しています
-    - 投稿者ごとのLGTM数を集計しています
+    - LGTM数順に記事を並べます
+    - 投稿者ごとの記事数を集計します
+    - 投稿者ごとのLGTM数を集計します
 
     **面白い結果がでること**間違いなしです。
     だって、キャンペーン自体が面白いのですもの！！！
@@ -72,19 +72,21 @@ defmodule Qiita.Qiitadelika202203 do
     **面白い結果**がでましたよね！！！
 
     般若心経の最初に「観自在菩薩」とあります。
-    データにおいてもまず「観」るということが出発点なのだとおもいます。
-    「観」る、つまり気づけたところから自在にデータを分析をすることでその結果として、**面白い結果**が得られるのです。
+    データにおいてもまず「観」ることが出発点なのだとおもいます。
+    「観」る、観察する、つまり気づきを出発点として、自在にデータを分析をすることでその終着として、**面白い結果**が得られるのです。
     みえる形にするにはまず集めることが必要です。
-    [イベントの概要](https://qiita.com/official-events/30be12dd14c0aad2c1c2#%E6%A6%82%E8%A6%81)に書いてある「**データの民主化**」に激しく同意いたします。
+    [イベントの概要](https://qiita.com/official-events/30be12dd14c0aad2c1c2#%E6%A6%82%E8%A6%81)に記載されている「**データの民主化**」に激しく同意いたします。
 
-    以下、この記事を自動更新しているプログラムについて補足しておきます。
+    ---
+
+    最後に、この記事を自動更新しているプログラムについて補足しておきます。
 
     - 自動更新は、[Elixir](https://elixir-lang.org/)というプログラミング言語がありまして、その[Elixir](https://elixir-lang.org/)で作られた[Nerves](https://www.nerves-project.org/)という[ナウでヤングでcoolなすごいIoTフレームワーク](https://www.slideshare.net/takasehideki/elixiriotcoolnerves-236780506)を使ってつくったアプリケーションで行っております
       - [Nerves](https://www.nerves-project.org/)の始め方につきましては下記の記事が詳しいです
       - [ElixirでIoT#4.1：Nerves開発環境の準備](https://qiita.com/takasehideki/items/88dda57758051d45fcf9)
     - [Elixir](https://elixir-lang.org/)には、データを自在に取り扱える[Enum](https://hexdocs.pm/elixir/Enum.html)モジュールがあります
-    - [Elixir](https://elixir-lang.org/)をはじめられる方は、[Enum](https://hexdocs.pm/elixir/Enum.html)モジュールの習得からはじめるとよいとおもいます
-    - [WEB+DB PRESS Vol.127](https://gihyo.jp/magazine/wdpress/archive/2022/vol127) :book: の特集２「Elixirによる高速なWeb開発！ 作って学ぶPhoenix」は、[Elixir](https://elixir-lang.org/)でWebアプリケーション開発を楽しめる[Phoenix](https://www.phoenixframework.org/)の基礎がガッシリ詰まっていて、**オススメ**です
+    - [Elixir](https://elixir-lang.org/)をはじめてみようという方は、[Enum](https://hexdocs.pm/elixir/Enum.html)モジュールの習得からはじめるとよいとおもいます
+    - [WEB+DB PRESS Vol.127](https://gihyo.jp/magazine/wdpress/archive/2022/vol127) :book: の特集２「Elixirによる高速なWeb開発！ 作って学ぶPhoenix」は、[Elixir](https://elixir-lang.org/)でWebアプリケーション開発を楽しめる[Phoenix](https://www.phoenixframework.org/)の基礎がぎっしりと詰まっていて、**オススメ**です
     - プログラムは、 https://github.com/TORIFUKUKaiou/hello_nerves/blob/f56229d372703d44819062bf81f8633905942a8a/lib/qiita/qiita_delika_202203.ex にあります
 
     https://github.com/TORIFUKUKaiou/hello_nerves/blob/f56229d372703d44819062bf81f8633905942a8a/lib/qiita/qiita_delika_202203.ex
