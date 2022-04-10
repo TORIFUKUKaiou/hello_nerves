4d2fe3c77890f2e3c06b441fc6a991da96b1234c
Masatoshi N <nishiguchi.masa@gmail.com>
2022-04-10 17:43:27 -0400

    Update nerves

--- mix.exs
+++ mix.exs
@@ -31,29 +31,31 @@ defmodule HelloNerves.MixProject do
   defp deps do
     [
       # Dependencies for all targets
-      {:nerves, "~> 1.7.4", runtime: false},
-      {:shoehorn, "~> 0.7.0"},
+      {:nerves, "~> 1.7.13", runtime: false},
+      {:shoehorn, "~> 0.9.0"},
       {:ring_logger, "~> 0.8.1"},
       {:toolshed, "~> 0.2.13"},
 
       # Dependencies for all targets except :host
-      {:nerves_runtime, "~> 0.11.3", targets: @all_targets},
-      {:nerves_pack, "~> 0.6.0", targets: @all_targets},
+      {:nerves_runtime, "~> 0.11.10", targets: @all_targets},
+      {:nerves_pack, "~> 0.7.0", targets: @all_targets},
 
       # Dependencies for specific targets
       # NOTE: It's generally low risk and recommended to follow minor version
       # bumps to Nerves systems. Since these include Linux kernel and Erlang
       # version updates, please review their release notes in case
       # changes to your application are needed.
-      {:nerves_system_rpi, "~> 1.17", runtime: false, targets: :rpi},
-      {:nerves_system_rpi0, "~> 1.17", runtime: false, targets: :rpi0},
-      {:nerves_system_rpi2, "~> 1.17", runtime: false, targets: :rpi2},
-      {:nerves_system_rpi3, "~> 1.17", runtime: false, targets: :rpi3},
-      {:nerves_system_rpi3a, "~> 1.17", runtime: false, targets: :rpi3a},
-      {:nerves_system_rpi4, "~> 1.17", runtime: false, targets: :rpi4},
-      {:nerves_system_bbb, "~> 2.12", runtime: false, targets: :bbb},
-      {:nerves_system_osd32mp1, "~> 0.8", runtime: false, targets: :osd32mp1},
-      {:nerves_system_x86_64, "~> 1.17", runtime: false, targets: :x86_64},
+      {:nerves_system_rpi, "~> 1.18", runtime: false, targets: :rpi},
+      {:nerves_system_rpi0, "~> 1.18", runtime: false, targets: :rpi0},
+      {:nerves_system_rpi2, "~> 1.18", runtime: false, targets: :rpi2},
+      {:nerves_system_rpi3, "~> 1.18", runtime: false, targets: :rpi3},
+      {:nerves_system_rpi3a, "~> 1.18", runtime: false, targets: :rpi3a},
+      {:nerves_system_rpi4, "~> 1.18", runtime: false, targets: :rpi4},
+      {:nerves_system_bbb, "~> 2.13", runtime: false, targets: :bbb},
+      {:nerves_system_osd32mp1, "~> 0.9", runtime: false, targets: :osd32mp1},
+      {:nerves_system_x86_64, "~> 1.18", runtime: false, targets: :x86_64},
+      {:nerves_system_npi_imx6ull, "~> 0.5", runtime: false, targets: :npi_imx6ull},
+      {:nerves_system_grisp2, "~> 0.2", runtime: false, targets: :grisp2},
       {:oauther, "~> 1.3"},
       {:extwitter, "~> 0.12.5"},
       {:circuits_gpio, "~> 1.0"},
