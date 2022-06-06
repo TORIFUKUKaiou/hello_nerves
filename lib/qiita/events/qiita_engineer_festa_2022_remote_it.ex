9953b82f6451c112ca4e9a01a8323d21d6f56eda
TORIFUKUKaiou <torifuku.kaiou@gmail.com>
2022-06-07 07:06:06 +0900

    https://qiita.com/official-events/8a2ab08f6fa0f093ae39

---
+++ lib/qiita/events/qiita_engineer_festa_2022_remote_it.ex
@@ -0,0 +1,17 @@
+defmodule Qiita.Events.QiitaEngineerFesta2022RemoteIt do
+  alias Qiita.Events.QiitaEngineerFesta2022RemoteIt.MarkdownGenerator
+  alias Qiita.Events.Repo
+
+  def run do
+    query = "tag:QiitaEngineerFesta_remote.it tag:remote.it"
+    start_time = DateTime.new(~D[2022-05-31], ~T[15:00:00.000], "Etc/UTC") |> elem(1)
+    end_time = DateTime.new(~D[2022-07-18], ~T[15:00:00.000], "Etc/UTC") |> elem(1)
+    tags = ["Elixir", "40代駆け出しエンジニア", "AdventCalendar2022", "QiitaEngineerFesta_remote.it", "remote.it"]
+    title = "【毎日自動更新】remote.it を使って○○に接続してみた！ LGTMランキング！"
+    id = "f284b5be23bc6f1a4e5e"
+
+    Repo.fetch_items(query, start_time, end_time)
+    |> MarkdownGenerator.generate()
+    |> Repo.post_markdown(tags, title, id)
+  end
+end
