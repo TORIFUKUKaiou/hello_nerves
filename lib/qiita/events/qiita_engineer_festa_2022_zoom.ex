708a5bf9f4e25ff6290ecebd4b79c797f2266a06
TORIFUKUKaiou <torifuku.kaiou@gmail.com>
2022-06-07 07:00:04 +0900

    typo

--- lib/qiita/events/qiita_engineer_festa_2022_zoom.ex
+++ lib/qiita/events/qiita_engineer_festa_2022_zoom.ex
@@ -7,7 +7,7 @@ defmodule Qiita.Events.QiitaEngineerFesta2022Zoom do
     start_time = DateTime.new(~D[2022-05-31], ~T[15:00:00.000], "Etc/UTC") |> elem(1)
     end_time = DateTime.new(~D[2022-07-18], ~T[15:00:00.000], "Etc/UTC") |> elem(1)
     tags = ["Elixir", "40代駆け出しエンジニア", "AdventCalendar2022", "QiitaEngineerFesta_Zoom", "Zoom"]
-    title = "【毎日自動更新】私が体験した1番ゾッとするインシデントの話 LGTMランキング！"
+    title = "【毎日自動更新】Zoom API/SDKを使ってみよう！ LGTMランキング！"
     id = "3b232899320ee2f1ceec"
 
     Repo.fetch_items(query, start_time, end_time)
