322bb50e06ae0e27503322c1078a7e74051cc179
TORIFUKUKaiou <torifuku.kaiou@gmail.com>
2022-10-06 21:54:44 +0900

    qiita events

--- lib/qiita/events/qiita_8e3542610897d988e66d.ex
+++ lib/qiita/events/qiita_8e3542610897d988e66d.ex
@@ -1,14 +1,14 @@
-defmodule Qiita.Events.Qiita5fdc281997d5754d8ac9 do
-  alias Qiita.Events.Qiita5fdc281997d5754d8ac9.MarkdownGenerator
+defmodule Qiita.Events.Qiita8e3542610897d988e66d do
+  alias Qiita.Events.Qiita8e3542610897d988e66d.MarkdownGenerator
   alias Qiita.Events.Repo
 
   def run do
-    query = "tag:フロントエンド"
+    query = "tag:PHP"
     start_time = DateTime.new(~D[2022-10-04], ~T[15:00:00.000], "Etc/UTC") |> elem(1)
     end_time = DateTime.new(~D[2022-11-01], ~T[15:00:00.000], "Etc/UTC") |> elem(1)
     tags = ["Elixir", "40代駆け出しエンジニア", "AdventCalendar2022"]
-    title = "【毎日自動更新】フロントエンドの開発効率を向上するヒントを教え合おう！ LGTMランキング！"
-    id = "93504a92bb68ec918a6a"
+    title = "【毎日自動更新】PHP強化月間～開発する上で知っておくべき知見を共有しよう～ LGTMランキング！"
+    id = "a50b97bd532cf971e867"
 
     Repo.fetch_items(query, start_time, end_time)
     |> MarkdownGenerator.generate()
