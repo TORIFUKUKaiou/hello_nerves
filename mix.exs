745f6c7ffc9f1765496fdc7f587c66b6fcf8045c
TORIFUKUKaiou <torifuku.kaiou@gmail.com>
2025-05-03 22:29:22 +0900

    元に戻す mix.exs

--- mix.exs
+++ mix.exs
@@ -34,7 +34,7 @@ defmodule HelloNerves.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger, :runtime_tools],
+      extra_applications: [:logger, :runtime_tools, :eex],
       mod: {HelloNerves.Application, []}
     ]
   end
@@ -71,7 +71,20 @@ defmodule HelloNerves.MixProject do
       {:nerves_system_osd32mp1, "~> 0.15", runtime: false, targets: :osd32mp1},
       {:nerves_system_x86_64, "~> 1.24", runtime: false, targets: :x86_64},
       {:nerves_system_grisp2, "~> 0.8", runtime: false, targets: :grisp2},
-      {:nerves_system_mangopi_mq_pro, "~> 0.6", runtime: false, targets: :mangopi_mq_pro}
+      {:nerves_system_mangopi_mq_pro, "~> 0.6", runtime: false, targets: :mangopi_mq_pro},
+      {:oauther, "~> 1.3"},
+      {:circuits_gpio, "~> 2.1"},
+      {:floki, "~> 0.36"},
+      {:flow, "~> 1.1"},
+      {:timex, "~> 3.7"},
+      {:quantum, "~> 3.4"},
+      {:pelemay, "~> 0.0.15"},
+      {:cpu_info, "~> 0.2.2"},
+      {:mix_tasks_upload_hotswap, "~> 0.1.2", only: :dev},
+      {:httpoison, "~> 2.2"},
+      {:jason, "~> 1.2"},
+      {:number, "~> 1.0.1"},
+      {:req, "~> 0.5"}
     ]
   end
 
