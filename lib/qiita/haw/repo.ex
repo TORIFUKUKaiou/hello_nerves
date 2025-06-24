9d26639b0ef22934a1a9d092ea1bf9051b5ea516
Awesome YAMAUCHI <torifuku.kaiou@gmail.com>
2025-06-24 09:50:55 +0900

    Simplify Qiita Haw Repo query

--- lib/qiita/haw/repo.ex
+++ lib/qiita/haw/repo.ex
@@ -1,6 +1,4 @@
 defmodule Qiita.Haw.Repo do
-  @users ~w(sgtakeru tsuruoka91 mnishiguchi torifukukaiou t_kamiya78 haw_ohnuma Erga-mion r-nakashio)
-  @organization_url_name "haw"
 
   def post_markdown(markdown, tags, title, id) do
     Qiita.Api.patch_item(
@@ -15,13 +13,10 @@ defmodule Qiita.Haw.Repo do
   def fetch_items do
     build_query()
     |> Qiita.Api.items()
-    |> Enum.filter(fn item -> Map.get(item, "organization_url_name") == @organization_url_name end)
   end
 
   defp build_query do
-    @users
-    |> Enum.map(&"user:#{&1}")
-    |> Enum.join(" or ")
+    "org:haw"
   end
 
   defp build_tags(tags) do
